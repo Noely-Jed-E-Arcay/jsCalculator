0711e51488ca462ba6fae38c14447ad4346f06c7
Noely-Jed-E-Arcay <drixgael@gmail.com>
2026-07-01 14:12:12 +0800

    added-third-number

--- js/script.js
+++ js/script.js
@@ -1,12 +1,50 @@
 function calculateSum(){
     const num1 = document.getElementById('num1').value;
     const num2 = document.getElementById('num2').value;
+    const num3 = document.getElementById('num3').value;
     
     const n1 = parseFloat(num1);
-    const n2 = parseFloat(num2)
+    const n2 = parseFloat(num2);
+    const n3 = parseFloat(num3);
     //Calculate Sum
-    const sum = n1 + n2;
+    const sum = n1 + n2 + n3;
 
     //display the result
-    document.getElementById('result').textContent='The sum is ' + sum;
+    document.getElementById('result').textContent='The sum is "' + sum + '"';
+}
+
+function calculateProduct(){
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+    const num3 = document.getElementById('num3').value;
+    
+    const n1 = parseFloat(num1);
+    const n2 = parseFloat(num2);
+    const n3 = parseFloat(num3);
+    //Calculate Sum
+    const product = n1 * n2 * n3;
+
+    //display the result
+    document.getElementById('result').textContent='The product is "' + product + '"';
+}
+
+function calculateQuotient(){
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+    const num3 = document.getElementById('num3').value;
+    
+    const n1 = parseFloat(num1);
+    const n2 = parseFloat(num2);
+    const n3 = parseFloat(num3);
+    //Calculate Sum
+    const quotient = n1 / n2 / n3;
+
+    //display the result
+    document.getElementById('result').textContent='The quotient is "' + quotient + '"';
+}
+
+function clear(){
+    const clear = getElementById('result');
+    //display the result
+    document.getElementById('result').textContent=clear;
 }
